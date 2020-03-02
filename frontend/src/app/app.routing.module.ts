639e5f0ba91e1d46fc5e9bcbe3527329cd189c9b
Bárbara C. Blank Garibaldi <barbaracbgaribaldi@gmail.com>
2020-03-02 10:22:51 -0300

    changes routing module

--- frontend/src/app/app.routing.module.ts
+++ frontend/src/app/app.routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+
+const appRoutes: Routes = [
+    { path: '', component: MoviesListComponent },
+    { path: 'movie/:id', component: MovieDetailComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+    )],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
